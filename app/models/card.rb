91c26854c5ff6c9aadcf861ef5e309e401a278c2
Mason Brown & Nadav Appel & Nicholas Wang & Prajay Soni <pair+codingmason.nadavp3.nwang9.prajaysoni@devbootcamp.com>
2016-07-15 12:12:01 -0400

    Add associations and validations to models

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,9 @@
 class Card < ActiveRecord::Base
   # Remember to create a migration!
+  belongs_to :deck
+
+  validates :question, presence: true
+  validates :answer, presence: true
+  validates :deck_id, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
+  has_many :stats
+  has_many :decks, through: :stats
+
+  has_secure_password
+
+  validates :username, uniqueness: true, presence: true
 end
